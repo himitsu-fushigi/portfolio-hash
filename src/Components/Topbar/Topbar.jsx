0d3f68be8654fe707c121fb1dffe3929aba40c64
Himitsu Fushigi <himitsu.fushigi@protonmail.com>
2021-08-25 11:47:19 +0545

    Image Loosely Compression

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -6,7 +6,7 @@ import Marquee from "react-fast-marquee";
 export default function Topbar() {
   return (
     <div className="topbar">
-      <Marquee gradient={false} loop={0} pauseOnHover={true} speed={35}>
+      <Marquee gradient={false} loop={0} pauseOnHover={true} speed={35} style={{textAlign: "center"}}>
         💼 Vice President of Operations by Profession @Hash Technologies, 🎓 Web Dev & Blockchain Researcher by Enthusiasm, 📸 Bike Rider & Landscape Photographer by Hobby.
       </Marquee>
     </div>
